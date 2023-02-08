622acd09406515996611a53fa1260b08eef8ded0
bharathinger <bharat3987@outlook.com>
2023-02-08 14:13:54 +0530

    Update `.gitignore`

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,23 +1,28 @@
 import React from "react";
 
 function GuessInput() {
-
-  const [guess, setGuess] = React.useState('');
+  const [guess, setGuess] = React.useState("");
   const handleChange = (event) => {
-    setGuess(event.target.value.toUpperCase())
-  }
+    setGuess(event.target.value.toUpperCase());
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     if (guess.length !== 5) {
-      window.alert('Please input word with only 5 characters! 🙃')
+      window.alert("Please input word with only 5 characters! 🙃");
       return;
     }
-    console.log({ guess })
-  }
+    console.log({ guess });
+    setGuess("");
+  };
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" value={guess} onChange={handleChange} />
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleChange}
+      />
     </form>
   );
 }
